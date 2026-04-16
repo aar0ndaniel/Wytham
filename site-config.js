fa2fa27d50e2d6d1e1b038c6b4c2873743159f8b
Aaron Daniel <76455241+aar0ndaniel@users.noreply.github.com>
2026-04-16 23:20:21 +0000

    Update site configuration with API base and key

--- site-config.js
+++ site-config.js
@@ -1,4 +1,4 @@
 window.WYTHAM_SITE_CONFIG = Object.freeze({
-  apiBase: '',
-  turnstileSiteKey: '',
+  apiBase: 'https://wytham-production.up.railway.app',
+  turnstileSiteKey: '0x4AAAAAAC-rgJsG3L9HVz3I',
 });
